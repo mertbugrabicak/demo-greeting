b1b216bd745a6eb07dbd26c2472ffbd5818a7d97
Mert Bugra Bicak <mbicak@redhat.com>
2022-04-06 11:16:24 +0200

    added greeting class

---
+++ src/main/java/com/mertbugrabicak/Greeting.java
@@ -0,0 +1,41 @@
+package com.mertbugrabicak;
+
+public class Greeting {
+
+    private long id;
+    private String greetingText;
+    private String greeter;
+
+    public Greeting() {
+    }
+
+    public Greeting(long id, String greetingText, String greeter) {
+        this.id = id;
+        this.greetingText = greetingText;
+        this.greeter = greeter;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getGreetingText() {
+        return greetingText;
+    }
+
+    public void setGreetingText(String greetingText) {
+        this.greetingText = greetingText;
+    }
+
+    public String getGreeter() {
+        return greeter;
+    }
+
+    public void setGreeter(String greeter) {
+        this.greeter = greeter;
+    }
+}
